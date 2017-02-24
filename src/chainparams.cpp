286c2ef69a1585370dca78b42ba01d9aaa28ca4a
Dev <admin@getneuron.com>
2017-02-25 11:27:51 +1300

    1

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -105,6 +105,32 @@ static void convertSeeds(std::vector<CAddress> &vSeedsOut, const unsigned int *d
     }
 }
 
+void MineGenesis(CBlock genesis, uint256 nProofOfWorkLimit){
+    // This will figure out a valid hash and Nonce if you're creating a differe$
+    uint256 hashTarget = nProofOfWorkLimit;
+    printf("Target: %s\n", hashTarget.GetHex().c_str());
+    uint256 newhash = genesis.GetHash();
+    uint256 besthash;
+    memset(&besthash,0xFF,32);
+    while (newhash > hashTarget) {
+        ++genesis.nNonce;
+        if (genesis.nNonce == 0){
+            printf("NONCE WRAPPED, incrementing time");
+            ++genesis.nTime;
+        }
+    newhash = genesis.GetHash();
+    if(newhash < besthash){
+        besthash=newhash;
+        printf("New best: %s\n", newhash.GetHex().c_str());
+    }
+    }
+    printf("Gensis Hash: %s\n", genesis.GetHash().ToString().c_str());
+    printf("Gensis Hash Merkle: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+    printf("Gensis nTime: %u\n", genesis.nTime);
+    printf("Gensis nBits: %08x\n", genesis.nBits);
+    printf("Gensis Nonce: %u\n\n\n", genesis.nNonce);
+}
+
 class CBaseChainParams : public CChainParams {
 public:
     CBaseChainParams() {
@@ -162,23 +188,12 @@ public:
 
         genesis.nBits    = bnProofOfWorkLimit.GetCompact();
         genesis.nNonce   = 411540;
+        hashGenesisBlock = genesis.GetHash();
 
-        hashGenesisBlock = uint256("0x01");
-        if (true && genesis.GetHash() != hashGenesisBlock)
-          {
-            Logprintf("recalculating params for mainnet.\n");
-            Logprintf("old mainnet genesis nonce: %s\n", genesis.nNonce.ToString().c_str());
-            Logprintf("old mainnet genesis hash:  %s\n", hashGenesisBlock.ToString().c_str());
-            // deliberately empty for loop finds nonce value.
-            for(genesis.nNonce == 0; genesis.GetHash() > bnProofOfWorkLimit; genesis.nNonce++){ }
-            Logprintf("new mainnet genesis merkle root: %s\n", genesis.hashMerkleRoot.ToString().c_str());
-            Logprintf("new mainnet genesis nonce: %s\n", genesis.nNonce.ToString().c_str());
-            Logprintf("new mainnet genesis hash: %s\n", genesis.GetHash().ToString().c_str());
-          }
+        if (true) { MineGenesis(genesis, nProofOfWorkLimit); }
 
-        // hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x8f9f7f85a22df9d62df2a3a11b7bcf376a14a0a16e217c16f7d71bc68c1b18b2"));
-        assert(genesis.hashMerkleRoot == uint256("0x8460d1df654f85d4626dffd47738db1a6cdc9df3a84e1ba361da39eacb479f46"));
+        assert(hashGenesisBlock == uint256("0x"));
+        assert(genesis.hashMerkleRoot == uint256("0x"));
 
         base58Prefixes[PUBKEY_ADDRESS]      = list_of(53).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[SCRIPT_ADDRESS]      = list_of(125).convert_to_container<std::vector<unsigned char> >();
